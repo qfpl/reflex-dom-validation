e9594f86b9404f68547145909f80a5a4ea804e91
Dave Laing <dave.laing.80@gmail.com>
2018-11-07 17:08:02 +1000

    Adds the beginning of a workflow helper.

---
+++ src/Reflex/Dom/Validation/Workflow.hs
@@ -0,0 +1,99 @@
+{-|
+Copyright   : (c) 2018, Commonwealth Scientific and Industrial Research Organisation
+License     : BSD3
+Maintainer  : dave.laing.80@gmail.com
+Stability   : experimental
+Portability : non-portable
+-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE TemplateHaskell #-}
+module Reflex.Dom.Validation.Workflow (
+    PageRequirement(..)
+  , _NoRequirement
+  , _SaveFirst
+  , _ValidateFirst
+  , WorkflowStep(..)
+  , WorkflowWidgetConfig(..)
+  , wwcBackRequirement
+  , wwcNextRequirement
+  , wwcTemplate
+  , workflowWidget
+  ) where
+
+import Control.Monad (void)
+import Data.Monoid (Endo(..))
+
+import Control.Lens
+
+import Reflex.Dom.Core
+import Data.Validation (toEither)
+
+import Reflex.Dom.Validation
+
+data PageRequirement =
+    NoRequirement
+  | SaveFirst
+  | ValidateFirst
+  deriving (Eq, Ord, Show, Read)
+
+makePrisms ''PageRequirement
+
+data WorkflowStep t m e f where
+  WorkflowStep :: Field t m e f f' -> WorkflowStep t m e f
+
+-- something about how to add the buttons to the workflow
+data WorkflowWidgetConfig t m f =
+  WorkflowWidgetConfig {
+    _wwcBackRequirement :: PageRequirement
+  , _wwcNextRequirement :: PageRequirement
+  , _wwcTemplate :: Bool -> Bool -> m (Event t (Endo (f Maybe))) -> m (Event t (), Event t (), Event t (Endo (f Maybe)))
+  }
+
+makeLenses ''WorkflowWidgetConfig
+
+workflowWidget :: MonadWidget t m
+               => WorkflowWidgetConfig t m f
+               -> [WorkflowStep t m e f]
+               -> ValidationWidget t m e f
+workflowWidget wwc [] i dv des =
+  pure never
+workflowWidget wwc steps i dv des =
+  let
+    l = length steps
+    w index =
+      let
+        step = steps !! index
+      in case step of
+        WorkflowStep f -> Workflow $ do
+          (eBack, eNext, eChange) <- (wwc ^. wwcTemplate) (index == 0) (index == (l - 1)) $
+            fieldWidget f i dv des
+
+          iv' <- sample . current $ dv
+          dv' <- foldDyn ($) iv' $ fmap appEndo eChange
+
+          let
+            eBackChange = case wwc ^. wwcBackRequirement of
+              NoRequirement -> mempty <$ eBack
+              _ -> never
+            eNextChange = case wwc ^. wwcNextRequirement of
+              NoRequirement -> mempty <$ eNext
+              _ -> never
+            checkValidation e =
+              let
+                (_, eSuccess) = fanEither $ toEither . fieldValidation f i <$> current dv' <@ e
+              in
+                void eSuccess
+            eBackW = case wwc ^. wwcBackRequirement of
+              ValidateFirst -> checkValidation eBack
+              _ -> eBack
+            eNextW = case wwc ^. wwcNextRequirement of
+              ValidateFirst -> checkValidation eNext
+              _ -> eNext
+
+            eChange' = leftmost [eBackChange, eNextChange, eChange]
+            eW = leftmost [w (index - 1) <$ eBackW, w (index + 1) <$ eNextW]
+
+          pure (eChange', eW)
+  in do
+    de <- workflow $ w 0
+    pure $ switchDyn de
