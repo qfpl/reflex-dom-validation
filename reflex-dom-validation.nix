9d8df97bce44051462f62b7da693f8144cba3124
Dave Laing <dave.laing.80@gmail.com>
2018-11-21 19:59:36 +1000

    Fixes up deps

--- reflex-dom-validation.nix
+++ reflex-dom-validation.nix
@@ -1,15 +1,15 @@
-{ mkDerivation, aeson, base, colour, containers, dependent-map
-, dependent-sum, errors, ghcjs-dom, jsaddle-warp, lens, mtl, reflex
-, reflex-dom, reflex-dom-storage, stdenv, text, time, validation
-, wai-app-static
+{ mkDerivation, aeson, base, bytestring, colour, containers
+, dependent-map, dependent-sum, errors, ghcjs-dom, jsaddle-warp
+, lens, mtl, reflex, reflex-dom, reflex-dom-storage, stdenv, text
+, time, validation, wai-app-static
 }:
 mkDerivation {
   pname = "reflex-dom-validation";
   version = "0.1.0.0";
   src = ./.;
   libraryHaskellDepends = [
-    aeson base colour containers dependent-map dependent-sum errors
-    ghcjs-dom jsaddle-warp lens mtl reflex reflex-dom
+    aeson base bytestring colour containers dependent-map dependent-sum
+    errors ghcjs-dom jsaddle-warp lens mtl reflex reflex-dom
     reflex-dom-storage text time validation wai-app-static
   ];
   license = stdenv.lib.licenses.bsd3;
